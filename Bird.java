648f71c721234a6cb3ab1fc2395bee6e6017c54b
Ignatius de Villiers <ignatius@entersekt.com>
2018-11-21 12:24:55 +0200

    Little Birdy version2.0

--- Bird.java
+++ Bird.java
@@ -1,17 +1,17 @@
 
 public class Bird {
 	
-	double X = 0.1;
-	double Y = 0.5;
+	volatile double x = 0.1;
+	volatile double y = 0.5;
 	double verticalVelocity = 0.0;
 	double gravity = 0.0020;
-	double jump = 0.030;  
+	double jump = 0.02;  
 	double radius = 0.055;
 	double degrees = 0;
 	double degchange = -1;
 	
 	public void flap() {
-		verticalVelocity =  jump; // set velocity to 'jump'
+		verticalVelocity = jump; // set velocity to 'jump'
 		degrees = 20; // rotation to 20 degrees
 	}
 
